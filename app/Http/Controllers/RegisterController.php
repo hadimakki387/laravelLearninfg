6f8ad19d147ebc7642b3ba81095936a2c5aef584
hadimakki387 <hmakki387@gmail.com>
2023-04-21 03:06:49 +0300

    added validation messages, sent a flash for the user when creating a new account

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use TijsVerkoyen\CssToInlineStyles\Css\Rule\Rule;
 
 class RegisterController extends Controller
 {
@@ -13,12 +14,14 @@ class RegisterController extends Controller
     public function store(){
         $attribute = request()->validate([
             'name' => ['required','max:255'],
-            'username' => ['required','max:255','min:3'],
-            'email' => ['required','email','max:255'],
+            'username' => ['required','max:255','min:3','unique:users,username'],
+            'email' => ['required','email','max:255','unique:users,email'],
             'password' => ['required','min:7','max:255'],
         ]);
 
         User::create($attribute);
+
+        session()->flash('success','Your account has been created.');
 
         return redirect('/');
     }
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -21,8 +21,14 @@
                         type="text"
                         name="name"
                         id="name"
+                        value="{{old('name')}}"
                         required
                     />
+                    @error('name')
+
+                    <p class="text-red-600">{{$message}}</p>
+                        
+                    @enderror
                 </div>
                 <div class="mb-6 grid">
                     <label
@@ -36,8 +42,14 @@
                         type="text"
                         name="username"
                         id="username"
+                        value="{{old('username')}}"
                         required
                     />
+                    @error('username')
+
+                    <p class="text-red-600">{{$message}}</p>
+                        
+                    @enderror
                 </div>
                 <div class="mb-6 grid">
                     <label
@@ -51,8 +63,14 @@
                         type="email"
                         name="email"
                         id="email"
+                        value="{{old('email')}}"
                         required
                     />
+                    @error('email')
+
+                    <p class="text-red-600">{{$message}}</p>
+                        
+                    @enderror
                 </div>
                 <div class="mb-6 grid">
                     <label
@@ -68,6 +86,11 @@
                         id="password"
                         required
                     />
+                    @error('password')
+
+                    <p class="text-red-600">{{$message}}</p>
+                        
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <button type="submit"
