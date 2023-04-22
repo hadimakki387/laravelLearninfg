123626abc962fef19d48a443f926626898bca629
hadimakki387 <hmakki387@gmail.com>
2023-04-22 23:54:56 +0300

    created the needed database for the comments

--- database/migrations/2023_04_22_204212_create_comments_table.php
+++ database/migrations/2023_04_22_204212_create_comments_table.php
@@ -11,16 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
-            $table->foreignId('category_id');
-            $table->string('title');
-            $table->text('excerpt');
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            //->constrained()->cascadeOnDelete(); this is for deleting all the comments
+            //when a post is deleted
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->text('body');
-            $table->text('slug');
             $table->timestamps();
-            $table->timestamp('published_at')->nullable();
         });
     }
 
@@ -29,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 };
